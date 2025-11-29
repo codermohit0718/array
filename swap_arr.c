908afd8f2117c17a0922055a5f3cfd5e458a0d8e
codermohit0718 <codermohit0718@gmail.com>
2025-11-29 18:43:29 +0530

    Add files via upload

---
+++ swap_arr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void fun(int arr[]){
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
+    return;
+}
+int main(){
+    int arr[2] = {1,2};
+    fun(arr);
+    printf("%d %d\n",arr[0],arr[1]);
+
+}
