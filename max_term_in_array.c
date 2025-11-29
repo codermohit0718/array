8cb9821e7857d62dcf84e180a56204fde624f168
codermohit0718 <codermohit0718@gmail.com>
2025-11-29 15:10:23 +0530

    Add files via upload

---
+++ max_term_in_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int arr[5] = {11,34,56,24,35};
+    int max = 0;
+    for(int i=0;i<=5;i++){
+        if(max<arr[i]){
+            max = arr[i];
+           
+        }
+    }
+     printf("the greatest number is:%d",max);
+     return 0;
+}
