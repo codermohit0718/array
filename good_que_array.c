7d5aa28bc0f3d6e8a321c2ee8218fe8b3e5e4e5e
codermohit0718 <codermohit0718@gmail.com>
2025-11-30 00:17:52 +0530

    Add files via upload

---
+++ good_que_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int totaldoublets = 0;
+    int arr[8] = {1,2,3,4,5,6,7,8};
+    int x = 10;
+    for(int i=0;i<8;i++){
+        for(int j=i+1;j<8;j++){
+            if(arr[i]+arr[j] == x){
+                totaldoublets++;
+                printf("%d %d\n",arr[i],arr[j]);
+            }
+
+        }
+    }
+    printf("%d",totaldoublets);
+    return 0;
+}
